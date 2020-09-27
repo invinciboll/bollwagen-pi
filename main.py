75684991a23e5efb01a63e71895fd6b3660a8b11
invinciboll <boll.basti@gmail.com>
2020-09-28 01:14:08 +0200

    modified rectangle, added button color change

--- main.py
+++ main.py
@@ -13,132 +13,125 @@ path = '/home/pi/Desktop/bw/'
 
 
 class tButton:
-    def __init__(self, index, name, grid, gpio):
+    def __init__(self, index, image, grid, gpio):
         self.index = index
-        self.name = name
+        self.image = image
         self.grid = grid
         self.gpio = gpio
-
-
-toggleButtons = []
-# index,name,grid,gpio
-toggleButtons.append(tButton(0, 'sign', [0, 0], 40))
-toggleButtons.append(tButton(1, 'outside', [1, 0], 38))
-toggleButtons.append(tButton(2, 'shots', [0, 1], 36))
 
 
 def general_callback(n):
     print('general_callback()', n)
     if GPIO.input(toggleButtons[n].gpio) == 0:
-        toggle_pushButtons[n].name = toggleButtons[n].name+'_on.png'
-        print(toggle_pushButtons[n].name)
+        toggle_pushButtons[n].image = toggleButtons[n].image+'_on.png'
         GPIO.output(toggleButtons[n].gpio, GPIO.HIGH)
     else:
-        toggle_pushButtons[n].name = toggleButtons[n].name+'_off.png'
-        print(toggle_pushButtons[n].name)
+        toggle_pushButtons[n].image = toggleButtons[n].image+'_off.png'
         GPIO.output(toggleButtons[n].gpio, GPIO.LOW)
 
+
+toggleButtons = []
+# index,image,grid,gpio
+toggleButtons.append(tButton(0, 'sign', [0, 0], 40))
+toggleButtons.append(tButton(1, 'outside', [1, 0], 38))
+toggleButtons.append(tButton(2, 'shots', [0, 1], 36))
 
 toggle_pushButtons = []
 for tButton in toggleButtons:
     GPIO.setup(tButton.gpio, GPIO.OUT)
     GPIO.output(tButton.gpio, GPIO.LOW)
     toggle_pushButtons.append(PushButton(app, args=[tButton.index], command=general_callback,
-                                         grid=tButton.grid, align='left', image=path + tButton.name+'_off.png'))
+                                         grid=tButton.grid, align='left', image=path + tButton.image+'_off.png'))
 
 ###########################################################################################################################################
 ### Menu Buttons ##########################################################################################################################
 
 
 class mTile:
-    def __init__(self, index, name, grid):
+    def __init__(self, index, image, grid):
         self.index = index
-        self.name = name
+        self.image = image
         self.grid = grid
 
         self.generateWindow()
         self.generatePicture()
         self.generateButtonBox()
-        self.generateCancleButton()
-        self.generateOkButton()
         self.generateTileButton()
 
     def generateWindow(self):
-        self.window = Window(app, title=self.name, bg="black", visible=False)
+        self.window = Window(app, title=self.image, bg="black", visible=False)
         self.window.tk.attributes("-fullscreen", True)
         self.window.tk.config(cursor='none')
 
     def generatePicture(self):
-        self.wPicture = Picture(self.window, image=self.name + '_off.png')
+        self.wPicture = Picture(self.window, image=self.image + '_off.png')
 
     def generateButtonBox(self):
         self.wButtonBox = Box(self.window, layout="auto",
                               align="bottom", width="fill")
-
-    def generateCancleButton(self):
-        self.wCancleButton = PushButton(
-            self.wButtonBox, align="right", image=path + "cancle.png", text="Close", command=self.window.hide)
-
-    def generateOkButton(self):
+        self.wCancleButton = PushButton(self.wButtonBox, align="right",
+                                        image=path + "cancle.png", text="Close", command=self.window.hide)
         self.wOkButton = PushButton(self.wButtonBox, align="right",
                                     image=path + "ok.png", text="Close", command=self.window.hide)
 
     def generateTileButton(self):
         self.button = PushButton(app, command=self.window.show,
-                                 grid=self.grid, align='left', image=path + self.name+'_off.png')
+                                 grid=self.grid, align='left', image=path + self.image+'_off.png')
 
 
 class mTileLed(mTile):
-    color = "#ffffff"
     r = 1,
     g = 1,
     b = 1,
 
-    def __init__(self, index, name, grid):
-        super().__init__(index, name, grid)
+    def __init__(self, index, image, grid):
+        super().__init__(index, image, grid)
         self.generateDrawing()
         self.generateSliders()
 
     def generateDrawing(self):
-        self.wDrawing = Drawing(self.window)
-        self.wDrawing.rectangle(0, 0, 400, 40, color=self.color)
+        self.wDrawing = Drawing(self.window, width="fill")
 
-    def calcColor(self):
+    def drawColor(self):
         self.wDrawing.rectangle(
-            0, 0, 400, 40, color=rgb2hex(self.r, self.g, self.b))
-        print("rgb2hex", rgb2hex(self.r, self.g, self.b))
+            20, 0, 460, 60, color=rgb2hex(self.r, self.g, self.b))
+        if(self.r > 0 or self.g > 0 or self.b or 0):
+            self.button.image = self.image + "_on.png"
+        else:
+            self.button.image = self.image + "_off.png"
 
     def sliderRedChanged(self, n):
         self.r = int(n)
-        self.calcColor()
+        self.drawColor()
 
     def sliderGreenChanged(self, n):
         self.g = int(n)
-        self.calcColor()
+        self.drawColor()
 
     def sliderBlueChanged(self, n):
         self.b = int(n)
-        self.calcColor()
+        self.drawColor()
 
     def generateSliders(self):
         self.wSliderRed = Slider(self.window, start=1, command=self.sliderRedChanged,
-                                 end=255, width="fill", height=40)
+                                 end=255, width="fill", height=60)
         self.wSliderRed.text_color = "red"
 
         self.wSliderGreen = Slider(self.window, start=1, command=self.sliderGreenChanged,
-                                   end=255, width="fill", height=40)
+                                   end=255, width="fill", height=60)
         self.wSliderGreen.text_color = "green"
 
         self.wSliderBlue = Slider(self.window, start=1, command=self.sliderBlueChanged,
-                                  end=255, width="fill", height=40)
+                                  end=255, width="fill", height=60)
         self.wSliderBlue.text_color = "blue"
 
 
 menuTiles = []
-#index, name, grid
+
+#index, image, grid
 menuTiles.append(mTileLed(0, 'ambient', [1, 1]))
-menuTiles.append(mTileLed(1, 'effect', [0, 2]))
-menuTiles.append(mTile(2, 'logo', [1, 2]))
+menuTiles.append(mTile(1, 'effect', [0, 2]))
+menuTiles.append(mTileLed(2, 'logo', [1, 2]))
 menuTiles.append(mTile(3, 'vent', [0, 3]))
 menuTiles.append(mTile(4, 'money', [1, 3]))
 
